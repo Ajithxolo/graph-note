5e51610360c023c0a3887fb71098839ab25ab8fb
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-12 13:28:44 +0530

    Adding test to return all notes when present

---
+++ spec/requests/fetch_notes_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe 'GraphQL fetchNotes query', type: :request do
+  let!(:note) { create(:note, title: "Test Note", body: "This is a test.") }
+
+  let(:query) do
+    <<-GRAPHQL
+      {
+        fetchNotes {
+          id
+          title
+          body
+        }
+      }
+    GRAPHQL
+  end
+  context 'when there are notes in the database' do
+    it 'returns a list of notes with the correct attributes' do
+      post '/graphql', params: { query: query }
+      json = JSON.parse(response.body)
+      expect(json['data']['fetchNotes']).to be_an(Array)
+    end
+  end
+end
