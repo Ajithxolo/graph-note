b0e5e183986f584e9c6d14bd305111e8cfba82e9
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-14 21:02:22 +0530

    Added test to return neutral score when neutral text is passed

--- spec/services/sentiment_analysis_service_spec.rb
+++ spec/services/sentiment_analysis_service_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe SentimentAnalysisService, type: :service do
     context 'when a valid text is passed' do
       let(:positive_text) { "I absolutely love this app, it's amazing!" }
       let(:negative_text) { "I absolutely hate this app, it's terrible!" }
+      let(:neutral_text) { "This app is okay, neither great nor bad." }
+
       it 'returns a sentiment score and label for positive sentiment', vcr: { cassette_name: "sentiment_analysis_positive" } do
         response = SentimentAnalysisService.new.analyze(positive_text)
         expect(response).to include(:sentiment_score, :sentiment_label)
@@ -19,6 +21,12 @@ RSpec.describe SentimentAnalysisService, type: :service do
         expect(response[:sentiment_score]).to be_a(Float)
         expect(response[:sentiment_score]).to eq(-1.0)
         expect(response[:sentiment_label]).to eq('negative')
+      end
+  
+      it 'returns a sentiment score and label for neutral sentiment', vcr: { cassette_name: "sentiment_analysis_neutral" } do
+        response = SentimentAnalysisService.new.analyze(neutral_text)
+        expect(response[:sentiment_score]).to eq(0.0)
+        expect(response[:sentiment_label]).to eq('neutral')
       end
     end
   end
