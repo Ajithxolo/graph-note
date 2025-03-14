1322bb006fddb1ae70c573c4b4aa81b134bd9e79
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-14 20:59:28 +0530

    Added code to return negative sentiment when a valid text is passed

--- spec/services/sentiment_analysis_service_spec.rb
+++ spec/services/sentiment_analysis_service_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe SentimentAnalysisService, type: :service, vcr: { cassette_name: "sentiment_analysis" } do
+RSpec.describe SentimentAnalysisService, type: :service do
   describe '#analyze' do
     context 'when a valid text is passed' do
       let(:positive_text) { "I absolutely love this app, it's amazing!" }
       let(:negative_text) { "I absolutely hate this app, it's terrible!" }
-      it 'returns a sentiment score and label' do
+      it 'returns a sentiment score and label for positive sentiment', vcr: { cassette_name: "sentiment_analysis_positive" } do
         response = SentimentAnalysisService.new.analyze(positive_text)
         expect(response).to include(:sentiment_score, :sentiment_label)
         expect(response[:sentiment_score]).to be_a(Float)
@@ -13,7 +13,7 @@ RSpec.describe SentimentAnalysisService, type: :service, vcr: { cassette_name: "
         expect(response[:sentiment_label]).to eq('positive')
       end
 
-      it 'returns a sentiment score and label for negative sentiment' do
+      it 'returns a sentiment score and label for negative sentiment', vcr: { cassette_name: "sentiment_analysis_negative" } do
         response = SentimentAnalysisService.new.analyze(negative_text)
         expect(response).to include(:sentiment_score, :sentiment_label)
         expect(response[:sentiment_score]).to be_a(Float)
