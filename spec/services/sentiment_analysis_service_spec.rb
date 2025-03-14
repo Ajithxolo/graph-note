48484a4ad9c35c02eebdeabe0aad8da19d729434
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-14 21:11:49 +0530

    Adding test to return error response when text is nil

--- spec/services/sentiment_analysis_service_spec.rb
+++ spec/services/sentiment_analysis_service_spec.rb
@@ -22,11 +22,21 @@ RSpec.describe SentimentAnalysisService, type: :service do
         expect(response[:sentiment_score]).to eq(-1.0)
         expect(response[:sentiment_label]).to eq('negative')
       end
-  
+
       it 'returns a sentiment score and label for neutral sentiment', vcr: { cassette_name: "sentiment_analysis_neutral" } do
         response = SentimentAnalysisService.new.analyze(neutral_text)
         expect(response[:sentiment_score]).to eq(0.0)
         expect(response[:sentiment_label]).to eq('neutral')
+      end
+    end
+
+    context 'when an invalid text is passed' do
+      let(:invalid_text) { nil }
+
+      it 'returns an error response for nil text' do
+        response = SentimentAnalysisService.new.analyze(invalid_text)
+        expect(response).to include(:error)
+        expect(response[:error]).to eq('Text must be present.')
       end
     end
   end
