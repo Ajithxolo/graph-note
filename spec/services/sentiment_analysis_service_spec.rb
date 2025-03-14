5fed4a56abb8a706bc4494180546b636a5d4444c
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-14 20:53:30 +0530

    Adding test to return a negative sentiment when a negative text is passed

--- spec/services/sentiment_analysis_service_spec.rb
+++ spec/services/sentiment_analysis_service_spec.rb
@@ -3,14 +3,22 @@ require 'rails_helper'
 RSpec.describe SentimentAnalysisService, type: :service, vcr: { cassette_name: "sentiment_analysis" } do
   describe '#analyze' do
     context 'when a valid text is passed' do
-      let(:sample_text) { "I absolutely love this app, it's amazing!" }
-
+      let(:positive_text) { "I absolutely love this app, it's amazing!" }
+      let(:negative_text) { "I absolutely hate this app, it's terrible!" }
       it 'returns a sentiment score and label' do
-        response = SentimentAnalysisService.new.analyze(sample_text)
+        response = SentimentAnalysisService.new.analyze(positive_text)
         expect(response).to include(:sentiment_score, :sentiment_label)
         expect(response[:sentiment_score]).to be_a(Float)
         expect(response[:sentiment_score]).to eq(1.0)
         expect(response[:sentiment_label]).to eq('positive')
+      end
+
+      it 'returns a sentiment score and label for negative sentiment' do
+        response = SentimentAnalysisService.new.analyze(negative_text)
+        expect(response).to include(:sentiment_score, :sentiment_label)
+        expect(response[:sentiment_score]).to be_a(Float)
+        expect(response[:sentiment_score]).to eq(-1.0)
+        expect(response[:sentiment_label]).to eq('negative')
       end
     end
   end
