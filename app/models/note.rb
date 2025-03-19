b9b4d59d6b467a909dc9cdc1e2719a32501855cf
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-19 19:57:34 +0530

    Integrate Elasticsearch into Note model with callbacks

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,5 @@
 class Note < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   validates :title, :body, :sentiment_score, :sentiment_label, presence: true
 end
