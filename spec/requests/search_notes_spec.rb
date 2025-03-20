65d1fb559996ea437ebc317313226f7441d199f5
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-20 11:10:41 +0530

    Adding test to return notes matching the search keyword when valid keyword is passed

---
+++ spec/requests/search_notes_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'GraphQL searchNotes query', type: :request do
+  let!(:note1) { create(:note, title: "GraphQL Guide", body: "Elasticsearch integration in Rails GraphQL.", sentiment_label: "neutral", sentiment_score: 0.0) }
+  let!(:note2) { create(:note, title: "Rails Testing", body: "TDD with RSpec and Capybara.", sentiment_label: "neutral", sentiment_score: 0.0) }
+  let!(:note3) { create(:note, title: "Elastic Power", body: "Full-text search using Elasticsearch, I'm very happy", sentiment_label: "positive", sentiment_score: 1.0) }
+
+  let(:query) do
+    <<-GRAPHQL
+      {
+        searchNotes(keyword: "GraphQL") {
+          id
+          title
+          body
+          sentimentScore
+          sentimentLabel
+        }
+      }
+    GRAPHQL
+  end
+
+  context 'when searching with a valid keyword' do
+    it 'returns notes matching the search keyword' do
+      post '/graphql', params: { query: query }
+      json = JSON.parse(response.body)
+
+      expect(json['data']['searchNotes']).to be_an(Array)
+      expect(json['data']['searchNotes'].size).to be >= 1
+      expect(json['data']['searchNotes'].first['title']).to include("GraphQL")
+    end
+  end
+end
